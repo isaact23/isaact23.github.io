917d3e82419683a03ae336fe401ae818004d0cbf
Isaac Thompson <shufflesninja@gmail.com>
2024-03-06 20:40:50 -0500

    Begin work on Angular static website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HomeComponent } from './home/home.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, ToolbarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'isaact-angular';
+  showHome = true;
 }
