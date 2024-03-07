2763c189028df486170a83b530225615e11c99ec
Isaac Thompson <shufflesninja@gmail.com>
2024-03-06 20:52:30 -0500

    Home page templating

---
+++ src/app/home/home-card.ts
@@ -0,0 +1,8 @@
+
+export interface HomeCard {
+  imgSrc: string;
+  imgAlt: string,
+  h1?: string,
+  h2?: string,
+  p?: string
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,22 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { HomeCard } from './home-card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  cards: HomeCard[] = [
+    {
+      imgSrc: "assets/img/isaac.jpg",
+      imgAlt: "Isaac Thompson",
+      h1: "Isaac Thompson",
+      h2: "isaact.net",
+      p: "Welcome! Feel free to browse my projects and websites."
+    }
+  ];
 }
